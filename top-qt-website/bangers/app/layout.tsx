8e1808cebfb20e934b7039719bea9ba239aca172
TheExGenesis <francisco.carvalho@ext.ec.europa.eu>
2025-11-26 17:56:12 +0000

    click tip

--- top-qt-website/bangers/app/layout.tsx
+++ top-qt-website/bangers/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Libre_Baskerville } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const libreBaskerville = Libre_Baskerville({
@@ -27,6 +28,7 @@ export default function RootLayout({
         className={`${libreBaskerville.variable} font-serif antialiased bg-white text-black`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
